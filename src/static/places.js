be12310e6eba4d6b96b1b6e409a2063c1c211048
runaiv <aruncoum@gmail.com>
2021-05-27 14:21:31 +0200

    snake landing page

--- src/static/places.js
+++ src/static/places.js
@@ -1,16 +1,16 @@
 const places = [
   {
-    title: 'Green',
+    title: 'Rule 2',
     description:
-      "The Maldives are home to some of the world's most ravishing islands, but it's the sea, which truly makes these islands shine. Luminous aquamarine waters with a crystal clarity lap upon these dazzling white shores, which barely peek above the Indian Ocean.",
-    imageUrl: process.env.PUBLIC_URL + '/assets/island1.jpg',
+      `Alors que le serpent avance inexorablement, le joueur ne peut que lui indiquer une direction à suivre (en haut, en bas, à gauche, à droite) afin d'éviter que la tête du serpent ne touche les murs ou son propre corps, auquel cas il risque de mourir.`,
+    imageUrl: process.env.PUBLIC_URL + '/assets/snake2.jpg',
     time: 1500,
   },
   {
-    title: 'Bora Bora',
+    title: 'Rule 1',
     description:
-      'Shaped like a giant sombrero, this lush volcanic island stars in countless South Pacific fantasies. The focal point and best asset of this tropical beauty is its ravishing lagoon in technicolor turquoise. Fish, turtles, sharks, and rays swim in the clear waters.',
-    imageUrl: process.env.PUBLIC_URL + '/assets/island2.jpg',
+      `Le joueur contrôle une longue et fine ligne semblable à un serpent, qui doit slalomer entre les bords de l'écran et les obstacles qui parsèment le niveau. Pour gagner chacun des niveaux, le joueur doit faire manger à son serpent un certain nombre de pastilles similaire à de la pomme, allongeant à chaque fois la taille du serpent.`,
+    imageUrl: process.env.PUBLIC_URL + '/assets/snake1.jpg',
     time: 1500,
   },
 ];
